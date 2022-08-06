4dbd10a2bd69633f45994d4e0223f5c00edc1285
Feliperios665 <110707050+Feliperios665@users.noreply.github.com>
2022-08-06 14:55:28 -0500

    Update Main.java
    cambio desde interfaz web

--- Multiplosde3QalabTesting/src/Main.java
+++ Multiplosde3QalabTesting/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /*
 * ejercicio para calcular si un numero 
 * ingresado por pantalla 
-* es milyiplo de 2 o 3
+* es milyiplo de 2 o 3.
 */
 public class Main {
     public static void main(String[] args) {
