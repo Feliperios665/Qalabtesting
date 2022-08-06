2cfe5c8fc2a0a6e8b55af6b7880f6b19318a666e
Feliperios665 <ariosgam@emeal.nttdata.com>
2022-08-06 14:06:32 -0500

    NegativoPositivo Add rama dev

---
+++ NegativoPositivo/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.print("Introduzca un número entero: ");
+        n = sc.nextInt();
+
+        if (n > 0) {
+            System.out.println("El número es positivo");
+        } else if(n < 0){
+            System.out.println("El número es negativo");
+        } else {
+            System.out.println("El número introducido es 0");
+        }
+    }
+}
